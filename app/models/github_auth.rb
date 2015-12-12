ba34a3b8f20a676d83c7c18104f3d9ab9100323f
David O'Keefe <email": null>
2015-12-12 15:38:23 -0600

    Add poro for github oauth authorization

---
+++ app/models/github_auth.rb
@@ -0,0 +1,13 @@
+class GithubAuth
+  attr_reader :github
+
+  def initialize
+    @github = Github.new(client_id: ENV['CLIENT_ID'],
+                          client_secret: ENV['CLIENT_SECRET'])
+  end
+
+  def authorize
+    github.authorize_url(redirect_uri: 'http://localhost:9393/callback',
+                          scope: 'user,repo')
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
 
+  def request_github_user(access_token)
+    client = Octokit::Client.new(:access_token => access_token)
+    client.user
+  end
+
 end
