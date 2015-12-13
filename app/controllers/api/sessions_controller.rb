bec79af8244c75da1a46d8d8523131f9f6e053d0
Matt Leo <email": null>
2015-12-13 11:14:59 -0600

    Add user session to sessions controller

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -8,6 +8,7 @@ class Api::SessionsController < ApplicationController
     access_token = GithubAccessToken.new(github_code).token
     user = GithubUser.new(access_token).user
     if user.save
+      session[:id] = user.id
       render json: user, status: 201
     else
       render json: { errors: user.errors.full_messages }
