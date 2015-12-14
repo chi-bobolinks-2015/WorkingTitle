c09bff2a94dddb07a177446e8d99d37bcf1d5053
Matt Leo <email": null>
2015-12-14 14:01:03 -0600

    Add create collection functionality for homepage

--- app/assets/javascripts/controllers/mainController.js
+++ app/assets/javascripts/controllers/mainController.js
@@ -2,14 +2,20 @@ var app = angular.module('sift', []);
 
   app.controller('mainController', ['$scope', '$http', function($scope, $http) {
     $scope.submit = function(){
-      console.log("submitting")
       var urls = $http.get('/api/search', { params: { keyword: $scope.keyword, paths: $scope.checkboxModel } })
       .success(function(urlData){
         $scope.urls = urlData
       })
     }
     $scope.submitCollection = function() {
+      var createCollection = $http.post('/collections.json', { collection: {collectionName: $scope.collection } } )
+        .success(function(createCollection){
+          console.log(createCollection)
+          $scope.newCollectionID = createCollection.id;
+          console.log($scope.newCollectionID)
+        })
       $scope.showCollectionForm = false;
+
     }
     $scope.showCollectionForm = function() {
       $scope.showCollectionForm = true;
--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,2 +1,15 @@
 class CollectionsController < ApplicationController
+  def create
+    collection = Collection.create(name: collection_params[:collectionName], user_id: current_user.id)
+
+    respond_to do |format|
+      format.json { render json: collection }
+    end
+  end
+
+  private
+
+  def collection_params
+    params.require(:collection).permit(:collectionName)
+  end
 end
