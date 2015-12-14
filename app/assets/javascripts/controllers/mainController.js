54d27f7f512235b33f4514855948cecefb999ed3
David O'Keefe <davidpokeefe3@gmail.com>
2015-12-14 09:49:17 -0600

    Add checkbox search selections for MVC options

--- app/assets/javascripts/controllers/mainController.js
+++ app/assets/javascripts/controllers/mainController.js
@@ -2,7 +2,8 @@ var app = angular.module('sift', []);
 
   app.controller('mainController', ['$scope', '$http', function($scope, $http) {
     $scope.submit = function(){
-      var urls = $http.get('/api/search', { params: { keyword: $scope.keyword } })
+      console.log($scope.checkboxModel)
+      var urls = $http.get('/api/search', { params: { keyword: $scope.keyword, paths: $scope.checkboxModel } })
       .success(function(urlData){
         $scope.urls = urlData
       })
