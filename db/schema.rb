1aee1fdfe4cec1a515b1f8275049d51c62206fc3
Lauren Markzon <lmarkzon@gmail.com>
2015-12-14 07:58:31 -0600

    Create collection/codeSnippet models/migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151212011327) do
+ActiveRecord::Schema.define(version: 20151214011800) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "code_snippets", force: :cascade do |t|
+    t.string   "code_url"
+    t.integer  "collection_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  create_table "collections", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.integer  "github_id"
