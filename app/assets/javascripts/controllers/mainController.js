b0a7513c310381408faee5d7cae65987cd8dfa7d
David O'Keefe <davidpokeefe3@gmail.com>
2015-12-14 10:52:39 -0600

    Add left nav bar to display organizations

--- app/assets/javascripts/controllers/mainController.js
+++ app/assets/javascripts/controllers/mainController.js
@@ -2,10 +2,14 @@ var app = angular.module('sift', []);
 
   app.controller('mainController', ['$scope', '$http', function($scope, $http) {
     $scope.submit = function(){
-      console.log($scope.checkboxModel)
       var urls = $http.get('/api/search', { params: { keyword: $scope.keyword, paths: $scope.checkboxModel } })
       .success(function(urlData){
         $scope.urls = urlData
       })
     }
+
+  var organizations = $http.get('/api/organizations')
+    .success(function(organizations){
+      $scope.orgs = organizations
+    })
   }])
