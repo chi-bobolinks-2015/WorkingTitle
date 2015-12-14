3172cf869c92ecebe67e17c694cd78eb9a15eb17
Matt Leo <email": null>
2015-12-14 11:08:42 -0600

    Add submit and save buttons

--- app/assets/javascripts/controllers/mainController.js
+++ app/assets/javascripts/controllers/mainController.js
@@ -2,9 +2,19 @@ var app = angular.module('sift', []);
 
   app.controller('mainController', ['$scope', '$http', function($scope, $http) {
     $scope.submit = function(){
+      console.log("submitting")
       var urls = $http.get('/api/search', { params: { keyword: $scope.keyword } })
       .success(function(urlData){
         $scope.urls = urlData
       })
+    }
+    $scope.submitCollection = function() {
+      $scope.showCollectionForm = false;
+    }
+    $scope.showCollectionForm = function() {
+      $scope.showCollectionForm = true;
+    }
+    $scope.showSearchForm = function() {
+      $scope.showSearchForm = true;
     }
   }])
