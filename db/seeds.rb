b42cfa51e995375f104b5089b5f1ed7db7a82b30
Angela Dyrda <adyrda2@gmail.com>
2015-12-14 14:09:21 -0600

    fix user show iteration

--- db/seeds.rb
+++ db/seeds.rb
@@ -34,27 +34,27 @@ code_snippets = [
   {:code_url => "https://github.com/adyrda2/github_oauth_example/blob/13015b872966112b45bd933a776c7051de301c2d/config/database.rb", :code_content => "var map;
   function initMap(){map = new google.maps.Map(document.getElementById('map'),{
   zoom: 4, center: {lat: -28, lng: 137}
-  });", :collection_id => 1 },
+  });", :collection_id => 2 },
   {:code_url => "https://github.com/adyrda2/github_oauth_example/blob/13015b872966112b45bd933a776c7051de301c2d/spec/band_spec.rb", :code_content => "var map;
   function initMap(){map = new google.maps.Map(document.getElementById('map'),{
   zoom: 4, center: {lat: -28, lng: 137}
-  });", :collection_id => 1 },
+  });", :collection_id => 2 },
   {:code_url => "https://github.com/adyrda2/phase-0-gps-1/blob/0ad4af3132ff281fcc65d363146c2b089933c6fa//awesome_page.md", :code_content => "var map;
   function initMap(){map = new google.maps.Map(document.getElementById('map'),{
   zoom: 4, center: {lat: -28, lng: 137}
-  });", :collection_id => 1 },
+  });", :collection_id => 3 },
   {:code_url => "https://github.com/adyrda2/adyrda2.github.io/blob/f79e74701cf4a7a17fa7270e10d4e788f530e224/blog/t2-css-design.html", :code_content => "var map;
   function initMap(){map = new google.maps.Map(document.getElementById('map'),{
   zoom: 4, center: {lat: -28, lng: 137}
-  });", :collection_id => 1 },
+  });", :collection_id => 4 },
   {:code_url => "https://github.com/adyrda2/adyrda2.github.io/blob/f79e74701cf4a7a17fa7270e10d4e788f530e224//index.html", :code_content => "var map;
   function initMap(){map = new google.maps.Map(document.getElementById('map'),{
   zoom: 4, center: {lat: -28, lng: 137}
-  });", :collection_id => 1 },
+  });", :collection_id => 7 },
   {:code_url => "https://github.com/adyrda2/adyrda2.github.io/blob/f79e74701cf4a7a17fa7270e10d4e788f530e224/blog/t7-Javascript.html", :code_content => "var map;
   function initMap(){map = new google.maps.Map(document.getElementById('map'),{
   zoom: 4, center: {lat: -28, lng: 137}
-  });", :collection_id => 1 },
+  });", :collection_id => 8 },
 ]
 
 CodeSnippet.create!(code_snippets)
