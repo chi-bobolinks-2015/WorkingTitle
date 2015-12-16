5b497e074da53a7258086b4754bd1d568c67a273
Matt Leo <email": null>
2015-12-15 18:38:05 -0600

    removed debugger

--- app/controllers/code_snippets_controller.rb
+++ app/controllers/code_snippets_controller.rb
@@ -1,6 +1,5 @@
 class CodeSnippetsController < ApplicationController
   def create
-    debugger
     code_snippet = CodeSnippet.create(code_url: snippet_params[:codeUrl], collection_id: snippet_params[:collectionId], code_content: snippet_params[:codeContent], path: snippet_params[:codePath])
 
     respond_to do |format|
