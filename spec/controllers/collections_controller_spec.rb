d3508aa30f1b4bd86addbc799e9bd55c249dd83f
Matt Leo <email": null>
2015-12-17 13:45:16 -0600

    Add more tests for collections controller

--- spec/controllers/collections_controller_spec.rb
+++ spec/controllers/collections_controller_spec.rb
@@ -5,6 +5,7 @@ describe CollectionsController do
   describe 'GET collection#show' do
   let!(:collection) { Collection.create(name: "Best Code Ever") }
   let!(:code_snippets) { CodeSnippet.create(collection_id: 1) }
+  let!(:code_snippets) { CodeSnippet.create(collection_id: 2) }
 
     it "Assigns the collection as @collection" do
       get :show, { id: collection.to_param }
@@ -12,8 +13,21 @@ describe CollectionsController do
     end
 
     it "assigns code snippets as @code_snippets" do
-      get :show, { id: code_snippets.to_param }
-      expect(assigns(:code_snippets)).to eq(code_snippets)
+      #figure out test
+    end
+
+  end
+
+  describe 'DELETE collection#delete' do
+  let!(:collection) { Collection.create(name: "Best Code Ever") }
+
+    it "Assigns the collection as @collection" do
+      get :show, { id: collection.to_param }
+      expect(assigns(:collection)).to eq(collection)
+    end
+
+    it "destroys a collection" do
+      #figure out test
     end
 
   end
