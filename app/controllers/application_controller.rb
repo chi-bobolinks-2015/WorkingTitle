e68989c84b9a419e985dfaf6b9b7507098f36ed4
David O'Keefe <davidpokeefe3@gmail.com>
2015-12-16 18:05:55 -0600

    Initial solution for multiple users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,10 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :null_session
+  # protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format == 'application/json' }
+  helper_method :current_user
 
   def current_user
-    @current_user ||= User.find_by(session[:id])
+    @current_user ||= User.find_by_id(session[:id])
   end
 end
