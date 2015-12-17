d9928fabd788869985ad84c18f2a34cc17938cbf
Matt Leo <email": null>
2015-12-17 11:28:10 -0600

    Add 2 rspec tests for collections controller, one failing

--- spec/controllers/collections_controller_spec.rb
+++ spec/controllers/collections_controller_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
-RSpec.describe CollectionsController, type: :controller do
+describe CollectionsController do
 
+  describe 'GET collection#show' do
+  let!(:collection) { Collection.create(name: "Best Code Ever") }
+  let!(:code_snippets) { CodeSnippet.create(collection_id: 1) }
+
+    it "Assigns the collection as @collection" do
+      get :show, { id: collection.to_param }
+      expect(assigns(:collection)).to eq(collection)
+    end
+
+    it "assigns code snippets as @code_snippets" do
+      get :show, { id: code_snippets.to_param }
+      expect(assigns(:code_snippets)).to eq(code_snippets)
+    end
+
+  end
 end
